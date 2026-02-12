c7ff00fd487cc711c9341cbfd20d3fc66009cccd
mehmetasilkilic <mehmetasil96@gmail.com>
2026-02-12 18:05:31 +0300

    fix: react example

--- examples/basic-react/vite.config.ts
+++ examples/basic-react/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom')
+    }
   }
 })
-
-
